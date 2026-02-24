22920def03d40b0b157d270fb3b7c3266a0c7ab7
pranayyy82 <pranaysai936@gmail.com>
2026-02-24 10:04:28 +0530

    Add files via upload

---
+++ UC5.java
@@ -0,0 +1,24 @@
+public class UC5 {
+    /*
+ * OOPSBannerApp UC5
+ * Inline Array Initialization using String.join()
+ */
+    public static void main(String[] args) {
+
+        String[] lines = {
+
+            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*     *", "*     "),
+            String.join("   ", "*     *", "*     *", "*     *", "*     "),
+            String.join("   ", "*     *", "*     *", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*      ", "     * "),
+            String.join("   ", "*     *", "*     *", "*      ", "*     *"),
+            String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
+
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
